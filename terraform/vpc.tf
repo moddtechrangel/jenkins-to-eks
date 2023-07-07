66e6efa77f3fdf4c3a1e72c6345393f23409722d
Rodolfo Angel <rangel@moddtech.com>
2023-07-07 16:03:12 -0700

    upgating module to version 5.0.0

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -2,7 +2,7 @@ data "aws_availability_zones" "azs" {}
 module "myapp-vpc" {
   source          = "terraform-aws-modules/vpc/aws"
 #  version         = "3.19.0"
-  version         = "4.67.0"
+  version         = "5.0.0"
   name            = "myapp-vpc"
   cidr            = var.vpc_cidr_block
   private_subnets = var.private_subnet_cidr_blocks
